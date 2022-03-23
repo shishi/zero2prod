3a6b02e0c684dc9a743cb33830a66b070fc849bf
Shigenobu Nishikawa <shishi@srevo.net>
2022-03-23 19:49:54 +0900

    finish 4.5.7

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use std::net::TcpListener;
+use env_logger::Env;
 use sqlx::PgPool;
-use zero2prod::startup::run;
+use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formating_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formating_layer);
+    set_global_default(subscriber).expect("Faied to set subscriber");
+
     let configuration = get_configuration().expect("Failed to load configuration");
     let connecton_pool = PgPool::connect(&configuration.database.connection_string()).await.expect("Failed to connect to database");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
+
     run(listener, connecton_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,14 +2,17 @@ use crate::routes::health_check;
 use crate::routes::subscribe;
 
 use actix_web::{web, App, HttpServer};
+use actix_web::web::Data;
 use actix_web::dev::Server;
 use std::net::TcpListener;
 use sqlx::PgPool;
+use actix_web::middleware::Logger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
