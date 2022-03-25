a93b07ba51567ac25d288410a4f674285661c7df
Shigenobu Nishikawa <shishi@srevo.net>
2022-03-25 20:17:08 +0900

    copy from official repo, I lost codes reinstall system

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
-use env_logger::Env;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formating_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formating_layer);
-    set_global_default(subscriber).expect("Faied to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Failed to load configuration");
-    let connecton_pool = PgPool::connect(&configuration.database.connection_string()).await.expect("Failed to connect to database");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-
-    run(listener, connecton_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,23 @@
+use once_cell::sync::Lazy;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
-use sqlx::{PgPool, Executor, Connection, PgConnection};
+use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
-use uuid::Uuid;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
 
 pub struct TestApp {
     pub address: String,
@@ -10,105 +25,124 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    Lazy::force(&TRACING);
+
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let mut configuration = get_configuration().expect("Failed to load configuration");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
-    let connecton_pool = configure_database(&configuration.database).await;
+    let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connecton_pool.clone())
-        .expect("Failed to bind address");
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-
     TestApp {
         address,
-        db_pool: connecton_pool,
+        db_pool: connection_pool,
     }
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    // Create database
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
-        .expect("Failed to connect to database");
+        .expect("Failed to connect to Postgres");
     connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
         .await
-        .expect("Failed to create database");
+        .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.connection_string())
+    // Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
-        .expect("Failed to connect to database");
+        .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
-        .expect("Failed to migrate database");
+        .expect("Failed to migrate the database");
 
     connection_pool
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    let app= spawn_app().await;
+    // Arrange
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
+    // Act
     let response = client
+        // Use the returned application address
         .get(&format!("{}/health_check", &app.address))
         .send()
         .await
-        .expect("Failed to send request");
+        .expect("Failed to execute request.");
 
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app= spawn_app().await;
+    // Arrange
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // Act
     let response = client
         .post(&format!("{}/subscriptions", &app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
-        .expect("Failed to execute request");
+        .expect("Failed to execute request.");
 
+    // Assert
     assert_eq!(200, response.status().as_u16());
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
-        .expect("Failed to fetch subscription");
-println!("{:?}", saved);
+        .expect("Failed to fetch saved subscription.");
+
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let app= spawn_app().await;
+    // Arrange
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
+        ("", "missing both name and email"),
     ];
 
-    for(invalid_body, error_message) in test_cases {
+    for (invalid_body, error_message) in test_cases {
+        // Act
         let response = client
-            .post (&format!("{}/subscriptions", &app.address))
-            .header ("Content-Type", "application/x-www-form-urlencoded")
-            .body (invalid_body)
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
             .send()
             .await
-            .expect("Failed to execute request");
+            .expect("Failed to execute request.");
 
-        assert_eq!(400, response.status().as_u16(), "The API did not fail with 400 Bad Request when the payload was {}.", error_message);
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
     }
-
 }
