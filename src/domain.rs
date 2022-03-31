f4fb4cb276dd3e314052cc5b787c5fabef680be7
Shigenobu Nishikawa <shishi@srevo.net>
2022-03-31 18:19:05 +0900

    finish 6.14

--- src/domain.rs
+++ src/domain.rs
@@ -1,29 +1,7 @@
-use unicode_segmentation::UnicodeSegmentation;
+mod new_subscriber;
+mod subscriber_email;
+mod subscriber_name;
 
-pub struct NewSubscriber {
-    pub email: String,
-    pub name: SubscriberName,
-}
-
-pub struct SubscriberName(String);
-
-impl SubscriberName {
-    pub fn parse(s: String) -> Result<SubscriberName, String> {
-        let is_empty_or_whitespace = s.trim().is_empty();
-        let is_too_long = s.graphemes(true).count() > 256;
-        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-        let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
-
-        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            panic!("{} is not valid subscriber name", s);
-        } else {
-            Ok(Self(s))
-        }
-    }
-}
-
-impl AsRef<str> for SubscriberName {
-    fn as_ref(&self) -> &str {
-        &self.0
-    }
-}
+pub use new_subscriber::NewSubscriber;
+pub use subscriber_email::SubscriberEmail;
+pub use subscriber_name::SubscriberName;
