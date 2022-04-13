9c9d1bf42f050682966652c28a3208f2aa01f848
Shigenobu Nishikawa <shishi@srevo.net>
2022-04-14 03:18:26 +0900

    10.2.5

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -108,20 +108,33 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, pool)
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+
+    match get_stored_credentials(&credentials.username, &pool)
         .await
         .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
+    {
+        Some((stored_user_id, stored_password_hash)) => {
+            user_id = Some(stored_user_id);
+            expected_password_hash = stored_password_hash;
+        }
+        _ => (),
+    }
 
-    let current_span = tracing::Span::current();
     spawn_blocking_with_tracing(move || {
-        current_span.in_scope(|| verify_password_hash(expected_password_hash, credentials.password))
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
-    .context("Failed to spawn blocking tas")
+    .context("Failed to spawn blocking task.")
     .map_err(PublishError::UnexpectedError)??;
 
-    Ok(user_id)
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
 }
 
 #[tracing::instrument(
