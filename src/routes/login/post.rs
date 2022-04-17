1f4614b9beb2e774d57e115c25f87faf46ccfc45
Shigenobu Nishikawa <shishi@srevo.net>
2022-04-18 01:03:41 +0900

    10.7.5.3

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,8 @@
-use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::error_chain_fmt;
-use actix_session::Session;
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    session_state::TypedSession,
+};
 use actix_web::web;
 use actix_web::HttpResponse;
 use actix_web::{error::InternalError, http::header::LOCATION};
@@ -21,7 +23,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -34,7 +36,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
