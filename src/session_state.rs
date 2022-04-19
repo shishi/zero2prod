dd0f94ad83787ba4747be5ae80922eefe025edb3
Shigenobu Nishikawa <shishi@srevo.net>
2022-04-20 01:40:57 +0900

    10.8.2.5

--- src/session_state.rs
+++ src/session_state.rs
@@ -20,13 +20,17 @@ impl TypedSession {
     pub fn get_user_id(&self) -> Result<Option<Uuid>, serde_json::Error> {
         self.0.get(Self::USER_ID_KEY)
     }
+
+    pub fn log_out(self) {
+        self.0.purge()
+    }
 }
 
 impl FromRequest for TypedSession {
     type Error = <Session as FromRequest>::Error;
     type Future = Ready<Result<TypedSession, Self::Error>>;
 
-    fn from_request(req: &HttpRequest, payload: &mut Payload) -> Self::Future {
+    fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
         ready(Ok(TypedSession(req.get_session())))
     }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{
-    admin_dashboard, change_password, change_password_form, confirm, health_check, login,
+    admin_dashboard, change_password, change_password_form, confirm, health_check, log_out, login,
     login_form, publish_newsletter, subscribe,
 };
 use crate::{configuration::DatabaseSettings, routes::home};
@@ -100,6 +100,7 @@ async fn run(
             .route("/admin/dashboard", web::get().to(admin_dashboard))
             .route("/admin/password", web::get().to(change_password_form))
             .route("/admin/password", web::post().to(change_password))
+            .route("/admin/logout", web::post().to(log_out))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
