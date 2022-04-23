91885de59b34abb1a3d250f0736f60e3f7594d33
Shigenobu Nishikawa <shishi@srevo.net>
2022-04-23 19:49:09 +0900

    10.9.1

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,12 +1,13 @@
 use crate::{
-    authentication::{validate_credentials, AuthError, Credentials},
+    authentication::{validate_credentials, AuthError, Credentials, UserId},
     utils::{e500, see_other},
 };
 use crate::{routes::admin::dashboard::get_username, session_state::TypedSession};
-use actix_web::{web, HttpResponse};
+use actix_web::{error::InternalError, web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -17,14 +18,10 @@ pub struct FormData {
 
 pub async fn change_password(
     form: web::Form<FormData>,
-    session: TypedSession,
     pool: web::Data<PgPool>,
+    user_id: web::ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = session.get_user_id().map_err(e500)?;
-    if user_id.is_none() {
-        return Ok(see_other("/login"));
-    };
-    let user_id = user_id.unwrap();
+    let user_id = user_id.into_inner();
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -33,7 +30,7 @@ pub async fn change_password(
         .send();
         return Ok(see_other("/admin/password"));
     }
-    let username = get_username(user_id, &pool).await.map_err(e500)?;
+    let username = get_username(*user_id, &pool).await.map_err(e500)?;
 
     let credentials = Credentials {
         username,
@@ -50,10 +47,21 @@ pub async fn change_password(
         };
     }
 
-    crate::authentication::change_password(user_id, form.0.new_password, &pool)
+    crate::authentication::change_password(*user_id, form.0.new_password, &pool)
         .await
         .map_err(e500)?;
 
     FlashMessage::error("Your password has been changed.").send();
     Ok(see_other("/admin/password"))
 }
+
+// async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
+//     match session.get_user_id().map_err(e500)? {
+//         Some(user_id) => Ok(user_id),
+//         None => {
+//             let response = see_other("/login");
+//             let e = anyhow::anyhow!("The user has not logged in.");
+//             Err(InternalError::from_response(e, response).into())
+//         }
+//     }
+// }
--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,16 @@
-use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{
     admin_dashboard, change_password, change_password_form, confirm, health_check, log_out, login,
     login_form, publish_newsletter, subscribe,
 };
+use crate::{authentication::reject_anonymous_users, configuration::Settings};
 use crate::{configuration::DatabaseSettings, routes::home};
 use actix_session::{storage::RedisSessionStore, SessionMiddleware};
 use actix_web::web::Data;
 use actix_web::{cookie::Key, dev::Server};
 use actix_web::{web, App, HttpServer};
 use actix_web_flash_messages::{storage::CookieMessageStore, FlashMessagesFramework};
+use actix_web_lab::middleware::from_fn;
 use secrecy::ExposeSecret;
 use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
@@ -90,17 +91,21 @@ async fn run(
                 secret_key.clone(),
             ))
             .wrap(TracingLogger::default())
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .route("/subscriptions/confirm", web::get().to(confirm))
-            .route("/newsletters", web::post().to(publish_newsletter))
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
-            .route("/admin/dashboard", web::get().to(admin_dashboard))
-            .route("/admin/password", web::get().to(change_password_form))
-            .route("/admin/password", web::post().to(change_password))
-            .route("/admin/logout", web::post().to(log_out))
+            .route("/health_check", web::get().to(health_check))
+            .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
+            .service(
+                web::scope("/admin")
+                    .wrap(from_fn(reject_anonymous_users))
+                    .route("/dashboard", web::get().to(admin_dashboard))
+                    .route("/password", web::get().to(change_password_form))
+                    .route("/password", web::post().to(change_password))
+                    .route("/logout", web::post().to(log_out)),
+            )
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
