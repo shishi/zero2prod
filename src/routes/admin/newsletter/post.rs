878782a850d622cb9b840c0b48a44a2ada0d79e3
Shigenobu Nishikawa <shishi@srevo.net>
2022-04-28 04:13:52 +0900

    11.9.2

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -2,7 +2,7 @@ use crate::{
     authentication::UserId,
     domain::SubscriberEmail,
     email_client::EmailClient,
-    idempotency::{get_saved_response, save_response, IdempotencyKey},
+    idempotency::{get_saved_response, save_response, try_processing, IdempotencyKey, NextAction},
     utils::{e400, e500, see_other},
 };
 use actix_web::web::ReqData;
@@ -38,6 +38,17 @@ pub async fn publish_newsletter(
         idempotency_key,
     } = form.0;
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+    match try_processing(&pool, &idempotency_key, *user_id)
+        .await
+        .map_err(e500)?
+    {
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => {
+            success_message().send();
+            return Ok(saved_response);
+        }
+    }
+
     if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
         .await
         .map_err(e500)?
@@ -66,12 +77,17 @@ pub async fn publish_newsletter(
             }
         }
     }
-    FlashMessage::info("The newsletter issue has been published!").send();
+
+    success_message().send();
     let response = see_other("/admin/newsletters");
     let response = save_response(&pool, &idempotency_key, *user_id, response)
         .await
         .map_err(e500)?;
     Ok(response)
+}
+
+fn success_message() -> FlashMessage {
+    FlashMessage::info("The newsletter issue has been published!")
 }
 
 struct ConfirmedSubscriber {
